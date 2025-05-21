e1fe92adb746e06326a767930fcd0ccf04301b78
Denley Hsiao <denley@justtodo.com>
2025-05-21 10:45:59 +0800

    Add oh-my-zsh default config.

---
+++ zsh/zshrc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# 
+# @author: denley@justtodo.com 
+# @date: 2025-05-21 10:32 
+# @description: 
+
+
+WORK_PATH=$(cd $(dirname $0); pwd)
+CONFIG_FILE=~/.zshrc
+
+[ -f ${CONFIG_FILE} ] && rm -f ${CONFIG_FILE}
+ln -s ${WORK_PATH}/oh-my-zshrc.vim ${CONFIG_FILE}
